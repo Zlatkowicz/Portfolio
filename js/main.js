5494f6497a36729aa967ed0529fac6707ac8c9c3
Armando <armandozlatkowicz@gmail.com>
2022-03-15 13:29:27 -0300

    Js example

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
-let name = prompt('Ingrese su nombre completo:');
-alert(`Bienvenido ${name}`);
+/* let numero = parseInt(prompt('Ingrese un numero:'));
+let resultado;
+for (let i = 0; i <= numero; i++) {
+    console.log(i);
+    if(numero > 1000) {
+        break;
+    }
+} */
