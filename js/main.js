8959abc1fcb83b666006869c4ee7a466a4d31810
Armando <armandozlatkowicz@gmail.com>
2022-03-17 14:55:15 -0300

    Code not finished

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-/* function pedirNumero() {
-    let num = parseInt(prompt('Ingrese un numero:'));
-    imprimirNumero(num)
-}
-function imprimirNumero(num) {
-    for(num >= 0; num < 100; num++) {
-        console.log("Hola");
-    }
-}
-pedirNumero(); */
-
-function darNombreCompleto(nombre, apellido) {
-    const nombreCompleto = `${nombre} ${apellido}`;
-    return nombreCompleto; /*  el return finaliza la funcion
-     ahí mismo, cualquier cosa debajo de return no sirve */
-}
-darNombreCompleto("Armando", "Zlatkowicz");
-/* let operador1 = parseInt(prompt('Ingrese un numero:'));
-let operador2 = parseInt(prompt('Ingrese otro numero:'));
-function sumar(x, y) {
-    return x + y;
-}
-const suma = sumar(operador1, operador2);
-alert(suma); */
-
 let userName = prompt('Ingrese su nombre:');
 let userSurename = prompt('Ingrese su apellido:');
 function fullNameFunction(name, surename) {
