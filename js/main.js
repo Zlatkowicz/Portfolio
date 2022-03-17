c47e6a0a22465a5409de0a02700325185fdfdabd
Armando <armandozlatkowicz@gmail.com>
2022-03-16 21:26:38 -0300

    Fullname function added

--- js/main.js
+++ js/main.js
@@ -15,3 +15,19 @@ function darNombreCompleto(nombre, apellido) {
      ahí mismo, cualquier cosa debajo de return no sirve */
 }
 darNombreCompleto("Armando", "Zlatkowicz");
+
+/* let operador1 = parseInt(prompt('Ingrese un numero:'));
+let operador2 = parseInt(prompt('Ingrese otro numero:'));
+function sumar(x, y) {
+    return x + y;
+}
+const suma = sumar(operador1, operador2);
+alert(suma); */
+
+let userName = prompt('Ingrese su nombre:');
+let userSurename = prompt('Ingrese su apellido:');
+function fullNameFunction(name, surename) {
+    return name + " " + surename;
+}
+const fullName = fullNameFunction(userName, userSurename);
+alert(`Bienvenido ${fullName}`);
